8ba1c5eabb243b53d57eb15c642ef5b6a0ab2f62
AmalKrishnaM <amal.krishna.m03@gmail.com>
2025-03-15 23:07:57 +0530

    Firebase.ts file has been added

---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+// src/app/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { environment } from '../../environments/environment'; // Import from environment file
+
+//check if firebase is already initialized. Otherwise initialize.
+const firebaseApp = getApps().length === 0 ? initializeApp(environment.firebase) : getApps()[0];
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+
